4e524c3f77d381ee2ed604588916f69310b97580
Joe Kutner <jpkutner@gmail.com>
2015-09-28 10:35:00 -0500

    Set version for next cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-invoke "0.1.0"
+(defproject lein-invoke "0.1.1-SNAPSHOT"
   :description "Invoke Leiningen tasks and test them"
   :url "https://github.com/jkutner/lein-invoke"
   :license {:name "Eclipse Public License"
